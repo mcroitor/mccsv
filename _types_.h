94894adb0b8638b161bad89e9215c816f067ecf0
Михаил Кройтор <m_croitor@hotmail.com>
2018-12-06 17:04:54 +0200

    add new changes

--- _types_.h
+++ _types_.h
@@ -9,6 +9,8 @@
 #define _TYPES__H
 
 #include <deque>
+#include <algorithm>
+#include <stdexcept>
 
 namespace mc {
     template<typename STRING_TYPE>
@@ -31,6 +33,7 @@ namespace mc {
         row_type values_;
     public:
         // methods
+
         column_t(cell_type header_name) : name_(header_name), values_() {
         }
 
@@ -91,19 +94,46 @@ namespace mc {
         table_t(const table_t<STRING_TYPE>& table);
         table_t(row_type headers);
         ~table_t();
-        
-        const row_type& header() const;
-        const column_type& column(size_t index) const;
-        const column_type& column(string column_name) const;
-        const column_type& operator [](string column_name) const;
-        column_type& operator [](size_t index);
-        
+
+        const row_type& header() const {
+            return header_;
+        }
+
+        const column_type& column(size_t index) const {
+            if(columns_.size() <= index){
+                throw std::out_of_range("out of range");
+            }
+            return columns_[index];
+        }
+
+        const column_type& column(string column_name) const {
+            auto index = std::find(header_.begin(), header_.end(), column_name);
+            if(index == header_.end()){
+                throw std::out_of_range("out of range");
+            }
+            return columns_[index - header_.begin()];
+        }
+
+        const column_type& operator[](string column_name) const {
+            return column(column_name);
+        }
+
+        const column_type& operator[](size_t index) const {
+            return column(index);
+        }
+        //        column_type& operator [](string column_name);
+        //        column_type& operator [](size_t index);
+
         // slow method!
         const row_type& row(size_t index) const;
-        
-        const cell_type& cell(size_t column_index, size_t row_index) const;
-        const cell_type& cell(string column_name, size_t row_index) const;
-        
+
+        const cell_type& cell(size_t column_index, size_t row_index) const{
+            return column(column_index)[row_index];
+        }
+        const cell_type& cell(string column_name, size_t row_index) const{
+            return column(column_name)[row_index];
+        }
+
         void insert_column(const column_type& column);
         // slow method!
         void insert_row(const row_type& row);
